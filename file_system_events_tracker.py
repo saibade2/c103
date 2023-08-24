d965af354ab176c83295a9de37adaa7c462dc0c5
Sai Bade <90086494+saibade2@users.noreply.github.com>
2023-08-24 18:05:49 +0530

    Update file_system_events_tracker.py

--- file_system_events_tracker.py
+++ file_system_events_tracker.py
@@ -7,50 +7,15 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
 from_dir = "C:/Users/dell/Downloads/sample"
-to_dir = "C:/Users/dell/OneDrive/Desktop/Document_Files"
-
-
-dir_tree = {
-    "Image_Files": ['.jpg', '.jpeg', '.png', '.gif', '.jfif'],
-    "Video_Files": ['.mpg', '.mp2', '.mpeg', '.mpe', '.mpv', '.mp4', '.m4p', '.m4v', '.avi', '.mov'],
-    "Document_Files": ['.ppt', '.xls', '.csv', '.pdf', '.txt'],
-    "Setup_Files": ['.exe', '.bin', '.cmd', '.msi', '.dmg']
-}
-
 
 
 class FileMovementHandler(FileSystemEventHandler):
+   
+   def on_created(self, event):
+      print(f"hey,{event.src_path} has been created")
 
-    def on_created(self, event):
-        name,extension=os.path.splitext(event.src_path)
-        time.sleep(1)
-
-        for key,value in dir_tree.items():
-            time.sleep(1)
-
-            if extension in value:
-                file_name=os.path.basename(event.src_path)
-
-                print("Download"+file_name)
-
-                path1=from_dir + '/' + file_name
-                path2=to_dir+'/'+key
-                path3=to_dir+'/'+key+'/'+file_name
-
-                if os.path.exists(path2):
-                    print("Directory Exists...")
-                    print("Moving"+file_name+"....")
-                    shutil.move(path1,path3)
-                    time.sleep(1)
-                else:
-                    print("Making Directory...")
-                    os.makedirs(path2)
-                    print("Moving"+file_name+"....")
-                    shutil.move(path1,path3)
-                    time.sleep(1)
-
-    
-
+   def on_deleted(self, event):
+        print(f"Oops! Someone deleted {event.src_path}!")
 
 
 event_handler = FileMovementHandler()
